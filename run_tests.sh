ebe866a2178601df861c252e60811962258a4235
Aman Orazaev <aorazaev@gmail.com>
2016-01-04 03:38:49 +0300

    Fix travis building

---
+++ run_tests.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+PYTHONPATH=bin python3 tests/input_output_tests.py
--- test/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(dirname `realpath $0`)
-
-PYTHONPATH=$SCRIPT_DIR/../bin python3 $SCRIPT_DIR/input_output_tests.py
